cc6f90df6809532b210c45d95095b925b7f479c6
vitormtz <vitor.brak@hotmail.com>
2022-12-05 17:13:41 -0300

    first commit

---
+++ src/main/java/gerenciadormongodb/Main.java
@@ -0,0 +1,14 @@
+package gerenciadormongodb;
+
+import gerenciadormongodb.apresentacao.FrmConsulta;
+
+/**
+ *
+ * @author vitor
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        new FrmConsulta().setVisible(true); 
+    }
+}
